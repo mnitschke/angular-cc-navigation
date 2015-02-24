81fbf6c9364cbf0c1d9facd8fea75e707fd6d4a4
marnits <m.nitschke@clearcode.cc>
2015-02-24 09:58:57 +0100

    gulp config

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    karma = require('gulp-karma');
+
+var testFiles = [
+    '/node_modules/angular/angular.js',
+    '/node_modules/angular-mocks/angular-mocks.js',
+    '/src/**/*.js',
+    '/tests/**/*.js'
+];
+
+gulp.task('scripts:minified', function() {
+    return gulp.src('./src/**/*.js')
+        .pipe(concat('angular-cc-logger.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('./src/**/*.js')
+        .pipe(concat('angular-cc-logger.js'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('test', function(){
+    return gulp.src(testFiles)
+        .pipe(karma({
+            configFile: 'karma.conf.js',
+            action: 'run'
+        }))
+        .on('error', function(err) {
+            throw err;
+        });
+});
+
+gulp.task('build',  ['test', 'scripts', 'scripts:minified']);
