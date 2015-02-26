ab88aa2a0204d5a34efefbb5dcdca772ae4ef5c9
marnits <m.nitschke@clearcode.cc>
2015-02-26 09:53:57 +0100

    Update .jshintrc

--- examples/app.js
+++ examples/app.js
@@ -22,20 +22,29 @@
         "priority": 200
     };
 
-    var child = {
-        "label": "Child",
-        "href": "/foobar",
+    var child_1 = {
+        "label": "Child 1",
+        "href": "/foochild",
         "priority": 205
     };
+
+    var child_2 = {
+        "label": "Child 2",
+        "href": "/childbar",
+        "priority": 210
+    }
 
     function AppConfig($navigationProvider) {
         $navigationProvider
             .add("tags", element_1)
-                .add("tags_child_1", child);
+                .add("tags_child_1", child_1);
         $navigationProvider
             .add("home", element_0);
         $navigationProvider
             .add("config", element_2);
+        $navigationProvider
+            .get("tags")
+                .add("tags_child_2", child_2);
     }
 
     AppConfig.$inject = [
